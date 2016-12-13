b97240d1e15644549d37e3f2c87ceb0f3b274519
iceper <105611899@qq.com>
2016-12-13 23:01:07 +0800

    commit frame

---
+++ src/main/java/algorithms/LFUCache.java
@@ -0,0 +1,21 @@
+package algorithms;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by iceper on 16/12/13.
+ */
+public class LFUCache {
+
+    public LFUCache(int capacity) {
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void set(int key, int value) {
+        return;
+    }
+}
